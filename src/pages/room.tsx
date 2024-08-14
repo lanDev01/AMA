b72d176663b747c4493f96e4cbc3fe75108741aa
LanDev01 <alnjr0001@outlook.com>
2024-08-14 17:36:53 -0300

    Feat: new send message function

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
-import { ArrowRight, Share2 } from "lucide-react";
+import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
 import amaLogo from "../assets/ama-logo.svg";
 import { Messages } from "../components/messages";
 import { Suspense } from "react";
+import { CreateMessageForm } from "../components/create-message-form";
 
 export function Room() {
   const { roomId } = useParams();
@@ -41,22 +42,7 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 focus-within:border-orange-400">
-        <input
-          type="text"
-          name="theme"
-          placeholder="Qual a sua pergunta?"
-          autoComplete="off"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-        />
-
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
-        >
-          Criar pergunta <ArrowRight className="size-4" />
-        </button>
-      </form>
+      <CreateMessageForm />
 
       <Suspense fallback={<p>Carregando...</p>}>
         <Messages />
