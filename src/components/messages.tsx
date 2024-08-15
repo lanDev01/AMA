a0e800ccf66fb6778b17af42249c0aca35884783
LanDev01 <alnjr0001@outlook.com>
2024-08-15 12:40:51 -0300

    Feat: Using hook and ordering message list when liking

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { Message } from "./message";
 import { getRoomMessages } from "../http/get-room-messages";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useMessagesWebSockets } from "../hooks/use-messages-websockets";
 
 export function Messages() {
   const { roomId } = useParams();
@@ -15,9 +16,15 @@ export function Messages() {
     queryFn: () => getRoomMessages({ roomId }),
   });
 
+  useMessagesWebSockets({ roomId })
+
+  const sortedMessages = data.messages.sort((a, b) => {
+    return b.amountOfReactions - a.amountOfReactions
+  })
+
   return (
     <ol className="list-decimal list-outside px-3 space-y-8">
-      {data.messages.map((message) => {
+      {sortedMessages.map((message) => {
         return (
           <Message
             key={message.id}
